07d095e5b49716d90614a3c650f2ab04032b41ef
Abdulloh <formygithubacc@gmail.com>
2020-06-07 00:46:09 +0500

    worked on chart.js to show info using line graph

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,7 +5,7 @@ import { Line, Bar } from "react-chartjs-2";
 import styles from "./Chart.module.css";
 
 export default function Cards() {
-  const [dailyData, setDailyData] = useState({});
+  const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
@@ -15,18 +15,28 @@ export default function Cards() {
     fetchAPI();
   });
 
-  const lineChart = dailyData[0] ? (
+  const lineChart = dailyData.length ? (
     <Line
       data={{
-        labels: "",
-        datasets: [{}, {}]
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255,0,0,0.5)",
+            fill: true
+          }
+        ]
       }}
     />
   ) : null;
 
-  return (
-    <div>
-      <h1>Chart </h1>
-    </div>
-  );
+  return <div className={styles.container}>{lineChart}</div>;
 }
